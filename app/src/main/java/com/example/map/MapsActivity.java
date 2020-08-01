fb8eec544226fa792b1ce303f54be679d6cca0eb
Safvan <safuputhalath@gmail.com>
2020-08-01 14:27:12 +0530

    Map

--- app/src/main/java/com/example/map/MapsActivity.java
+++ app/src/main/java/com/example/map/MapsActivity.java
@@ -6,40 +6,108 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback , LocationListener {
 
     private GoogleMap mMap;
     private static final int LOCATION_REQUEST =500;
+    private  final int MIN_TIME =1000;
+    private  final int MIN_DISTANCE =1;
 
+    FirebaseAuth firebaseAuth;
+   private DatabaseReference reference;
+    LocationManager locationManager;
+    Marker marker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        reference= FirebaseDatabase.getInstance().getReference().child("User-101");
+        locationManager= (LocationManager) getSystemService(LOCATION_SERVICE);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        getLocationUpdate();
+        readChanges();
     }
 
+
+    private void readChanges() {
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()){
+                    try {
+                        MyLocation myLocation=dataSnapshot.getValue(MyLocation.class);
+                        if (myLocation!=null){
+                       marker.setPosition(new LatLng(myLocation.getLatitude(),myLocation.getLongitude()));
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void getLocationUpdate() {
+        if (locationManager!=null){
+            if (ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED &&ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION)==PackageManager.PERMISSION_GRANTED) {
+                { if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
+                {locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER,MIN_TIME,MIN_DISTANCE,this);}
+                else if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,MIN_TIME,MIN_DISTANCE,this);
+                }else {
+                 Toast.makeText(this,"NO Provider Enabled..",Toast.LENGTH_SHORT).show();
+                }}
+            }else{
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},101);
+            }
+        }
+    }
 
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        LatLng splace=new LatLng(31,74);
+        marker=mMap.addMarker(new MarkerOptions().position(splace).title("Mark in my Location"));
+    //    mMap.setMinZoomPreference(12);
         mMap.getUiSettings().setZoomControlsEnabled(true);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(this,new String[]{ Manifest.permission.ACCESS_FINE_LOCATION},LOCATION_REQUEST);
-            return;
-        }
-        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setAllGesturesEnabled(true);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(splace));
+//        mMap.getUiSettings().setZoomControlsEnabled(true);
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+//            ActivityCompat.requestPermissions(this,new String[]{ Manifest.permission.ACCESS_FINE_LOCATION},LOCATION_REQUEST);
+//            return;
+//        }
+//        mMap.setMyLocationEnabled(true);
 
        }
 
@@ -49,9 +117,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         {
             case LOCATION_REQUEST:
                 if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    mMap.setMyLocationEnabled(true);
-                }
+                 getLocationUpdate();                }
                 break;
         }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if (location!=null){
+            saveLocation(location);
+        }else {
+            Toast.makeText(getApplicationContext(),"No Loction",Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void saveLocation(Location location) {
+        reference.setValue(location);
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
